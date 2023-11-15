f3fa1f5fe640c7acc3713531e00bb28429d176a8
Edward Wesson <itswesson@gmail.com>
2023-11-15 15:50:52 -0600

    Ten Functions Progress

--- solutions.js
+++ solutions.js
@@ -21,3 +21,104 @@
  * concat
  */
 
+// Question 1
+function isTrue(input) {
+    return input === true
+}
+//  const isTrue = input => input === true;
+
+
+// Question 2
+// Define a function named 'isFalse'
+// takes in a value
+// returns true if input exactly equal to false
+
+function isFalse(input) {
+    return input === false
+}
+
+// Question 3
+// define a function named 'not'
+// takes in input
+// returns boolean opposite of input
+
+function not(input) {
+    // return input === (!false)
+    return !input
+}
+
+
+// Question 4
+// define a function named "addOne"
+// enter single input
+// returns input +1
+
+function addOne(input) {
+    return Number(input) + (1)
+}
+// function addOne(input) {
+//     if (!isNaN(input)) {
+//         return Number(input) + 1;
+//     } else {
+//         return "Invalid input. Please provide a number or a numeric string.";
+//     }
+// }
+
+// Question 5
+// function isEven(input) {
+//     // // guard clause against booleans
+//     if(typeof (input) == "boolean") {
+//         return false
+//      }
+//     if(typeof (input) == "string") {
+//         return false
+//     }
+//     if (input % 2 === 0) {
+//         return true
+//     }
+//     if (input % 2 === 1) {
+//         return false
+//     }
+
+    // // #5 example 1
+    // const isEven = (input) => {
+    //     if(
+    //         typeof input === 'number'
+    //         || typeof input === 'string'
+    //         && !isNaN(input)
+    //     ){
+    //     return input % 2
+    //     }
+    // }
+    // }
+
+//  // #5 example 2
+const isEven = (input) => {
+    if(typeof input === 'number')
+        return input % 2 === 0;
+    if(typeof input === 'boolean')
+        return false;
+    if(typeof input === 'string')
+        if(!isNaN(input)) {
+            return input % 2 === 0
+        } else {
+            return false
+        }
+}
+
+//     #5 example 3
+// if(typeof input === 'boolean') {
+//     return false;
+// }
+// return input % 2 === 0;
+
+// Question 6
+
+
+// Question 10
+function concat(input1, input2) {
+    if(
+        typeof input1 === 'string'
+        && typeof input2 === 'string'
+    )    {}
+}
